5df70e164939a66fdbb99e6230f7674f05389eb3
bengi-gnc <bngigenc@gmail.com>
2022-10-13 13:42:46 +0300

    categories bölümü oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:food_app/buton.dart';
+import 'package:food_app/categories.dart';
 import 'package:food_app/food1.dart';
 import 'package:food_app/food2.dart';
 import 'package:food_app/location.dart';
@@ -71,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
    
     return Scaffold(
 
-      body: location()
+      body: Categories()
     );
   }
 }
